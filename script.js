0907b1030dfcfcece7643364814c1a28912d2628
Emma <alunia.moon@gmail.com>
2023-06-30 16:31:13 +0200

    Function to show quiz questions and answers
    Fn checking the quiz index and showing the question and answers
    on that index in the array.

--- script.js
+++ script.js
@@ -42,3 +42,35 @@ let quiz = [
     ],
   },
 ];
+
+
+// quizIndex stores the updated quiz index from inside the function so it isn't lost
+let quizIndex = 0;
+let mainContentElement = document.querySelector(".main-content");
+
+// showQuizQuestions function to show the current main text content
+let showQuizQuestions = () => {
+  // currentQuestionObject is our new source of truth for quiz question index
+  let currentQuestionObject = quiz[quizIndex];
+
+  // shows the paragraph tag with main-text class, adds the question text from my array at the current index stored in currentQuestionObject
+  let template = `<p class="main-text">${currentQuestionObject.questionText}</p>`;
+
+  // creates an answersButton variable with an empty string
+  let answersButton = "";
+
+  //
+  let answersArray = currentQuestionObject.answers;
+  for (let answerIndex = 0; answerIndex < answersArray.length; answerIndex++) {
+    // new source of truth for quiz answer index
+    let currentAnswer = answersArray[answerIndex];
+
+    //
+    let button = `<button class="options">${currentAnswer.answerText}</button>`;
+
+    // appends all answers created from the currentAnswer object
+    answersButton += button;
+  }
+
+  mainContentElement.innerHTML = template + answersButton;
+};
