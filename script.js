71c27b8c955ba50d24959ae3a73954ed8877254d
Emma <alunia.moon@gmail.com>
2023-06-30 21:04:40 +0200

    Add remaining questions and answers to the array

--- script.js
+++ script.js
@@ -24,19 +24,188 @@ let quiz = [
     questionText: "What is Mando's real name from The Mandalorian?",
     answers: [
       {
-        answerText: "Boba Fett.",
+        answerText: "Boba Fett",
         isTrue: false,
       },
       {
-        answerText: "Han Solo.",
+        answerText: "Han Solo",
         isTrue: false,
       },
       {
-        answerText: "Din Djarin.",
+        answerText: "Din Djarin",
         isTrue: true,
       },
       {
         answerText: "Joel",
+        isTrue: false,
+      },
+    ],
+  },
+  {
+    questionText: "Han Solo was frozen in what?",
+    answers: [
+      {
+        answerText: "Time",
+        isTrue: false,
+      },
+      {
+        answerText: "Carbonite",
+        isTrue: true,
+      },
+      {
+        answerText: "Jelly",
+        isTrue: false,
+      },
+      {
+        answerText: "A Galaxy Far, Far Away",
+        isTrue: false,
+      },
+    ],
+  },
+  {
+    questionText: "Finish this quote from The Mandalorian: 'This is the ___'",
+    answers: [
+      {
+        answerText: "end",
+        isTrue: false,
+      },
+      {
+        answerText: "story",
+        isTrue: false,
+      },
+      {
+        answerText: "mando",
+        isTrue: false,
+      },
+      {
+        answerText: "way",
+        isTrue: true,
+      },
+    ],
+  },
+  {
+    questionText: "Lightsabers are powered by what type of crystal?",
+    answers: [
+      {
+        answerText: "Kyber crystal",
+        isTrue: true,
+      },
+      {
+        answerText: "Light crystal",
+        isTrue: false,
+      },
+      {
+        answerText: "Quartz Crystal",
+        isTrue: false,
+      },
+      {
+        answerText: "Diamond",
+        isTrue: false,
+      },
+    ],
+  },
+  {
+    questionText: "Who is Darth Vader's grandson?",
+    answers: [
+      {
+        answerText: "Obi-Wan Kenobi",
+        isTrue: false,
+      },
+      {
+        answerText: "Han Solo",
+        isTrue: false,
+      },
+      {
+        answerText: "Luke Skywalker",
+        isTrue: false,
+      },
+      {
+        answerText: "Kylo Ren",
+        isTrue: true,
+      },
+    ],
+  },
+  {
+    questionText: "What episode is The Phantom Menace?",
+    answers: [
+      {
+        answerText: "I",
+        isTrue: true,
+      },
+      {
+        answerText: "III",
+        isTrue: false,
+      },
+      {
+        answerText: "VI",
+        isTrue: false,
+      },
+      {
+        answerText: "VII",
+        isTrue: false,
+      },
+    ],
+  },
+  {
+    questionText: "Where is Jabba the Hutt's Palace located?",
+    answers: [
+      {
+        answerText: "Alderaan",
+        isTrue: false,
+      },
+      {
+        answerText: "Endor",
+        isTrue: false,
+      },
+      {
+        answerText: "Coruscant",
+        isTrue: false,
+      },
+      {
+        answerText: "Tatooine",
+        isTrue: true,
+      },
+    ],
+  },
+  {
+    questionText:
+      "According to Luke, confronting what is the destiny of a Jedi?",
+    answers: [
+      {
+        answerText: "Their Fear of Heights",
+        isTrue: false,
+      },
+      {
+        answerText: "The Dark Side of the Coffee",
+        isTrue: false,
+      },
+      {
+        answerText: "Public Speaking",
+        isTrue: false,
+      },
+      {
+        answerText: "Fear",
+        isTrue: true,
+      },
+    ],
+  },
+  {
+    questionText: "Bo-Katan wanted what from Moff Gideon in The Mandalorian?",
+    answers: [
+      {
+        answerText: "A Pair of Mandalorian Boots",
+        isTrue: false,
+      },
+      {
+        answerText: "The Darksaber",
+        isTrue: true,
+      },
+      {
+        answerText: "Gideon's Secret Chicken Recipe",
+        isTrue: false,
+      },
+      {
+        answerText: "The Map to Luke Skywalker's location",
         isTrue: false,
       },
     ],
