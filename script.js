1e547728a45d48617e7041d09deccb7477ffa92e
Emma <alunia.moon@gmail.com>
2023-07-01 22:22:44 +0200

    Add restart game button and write comments
    Define parameters of if statement tracking if the currentQuestionObject
     is undefined and creating a congratulations message
     along with a button to restart the game (which
     then calls the function again after resetting the quizIndex)

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// array of quiz objects containing a question, 4 answers, and a property telling if the answer is true or not
+
 let quiz = [
   {
     questionText: "What is Baby Yoda's real name?",
@@ -214,37 +216,63 @@ let quiz = [
 
 // quizIndex stores the updated quiz index from inside the function so it isn't lost
 let quizIndex = 0;
+
+// targets the element with the class of main-content
 let mainContentElement = document.querySelector(".main-content");
 
 // showQuizQuestions function to show the current main text content
 let showQuizQuestions = () => {
+  
   // currentQuestionObject is our new source of truth for quiz question index
   let currentQuestionObject = quiz[quizIndex];
 
-  // shows the paragraph tag with main-text class, adds the question text from my array at the current index stored in currentQuestionObject
-  let template = `<p class="main-text">${currentQuestionObject.questionText}</p>`;
+  // once the currentQuestionObject reaches the end of the array it will return undefined, this if statement utilises that attribute to end the game
+  if (currentQuestionObject === undefined) {
 
-  // creates an answersButton variable with an empty string
-  let answersButton = "";
+    // resetting the quizIndex so the player can restart the game 
+    quizIndex = 0;
+    
+    // add congratulatory message
+    let congratulationsParagraph = `<p>Well done for completing the quiz! Your score is shown below.</p>`;
 
-  //
-  let answersArray = currentQuestionObject.answers;
-  for (let answerIndex = 0; answerIndex < answersArray.length; answerIndex++) {
-    // new source of truth for quiz answer index
-    let currentAnswer = answersArray[answerIndex];
+    // add restart game button that runs the function over from the beginning thanks to the resetting of quizIndex above
+    let restartButton = `<button class="options" onclick="showQuizQuestions()">Restart game</button>`;
 
-    //
-    let button = `<button class="options" onclick="showQuizQuestions()">${currentAnswer.answerText}</button>`;
-
-    // appends all answers created from the currentAnswer object
-    answersButton += button;
+    // appends the p and button tags to the page
+    mainContentElement.innerHTML = congratulationsParagraph + restartButton;
   }
 
-  mainContentElement.innerHTML = template + answersButton;
+  // shows the paragraph tag with main-text class, adds the question text from my array at the current index stored in currentQuestionObject
+  let questionTextElement = `<p class="main-text">${currentQuestionObject.questionText}</p>`;
+
+  // creates an answerButtons variable with an empty string
+  let answerButtons = "";
+
+  // variable tracking the iteration of the answers connected to the currentQuestionObject
+  let answersArray = currentQuestionObject.answers;
+
+  // creates answers buttons for each answer, runs until all answerText have been appended to a button and sent to the answerButtons list
+  for (let answerIndex = 0; answerIndex < answersArray.length; answerIndex++) {
+
+    // new source of truth for currentQuestionObject => answersArray based on the answerIndex
+    let currentAnswer = answersArray[answerIndex];
+
+    // takes the text from the answerText in the currentAnswer object and creates a button
+    let button = `<button class="options" onclick="showQuizQuestions()">${currentAnswer.answerText}</button>`;
+
+    // appends the answerText button one at a time to the answerButtons list
+    answerButtons += button;
+  }
+
+  // appends the created p tag in questionTextElement, and the answer buttons stored in the answerButtons list
+  mainContentElement.innerHTML = questionTextElement + answerButtons;
 
   // quizIndex++ adds 1 to the quizIndex much like a for loop, so that the next index shows when a button is clicked
   quizIndex++;
 };
 
+// startGame targets the element with the class name start-game
 let startGame = document.querySelector(".start-game");
+
+// event listener is listening for click event to run the showQuizQuestions function
 startGame.addEventListener("click", showQuizQuestions);
